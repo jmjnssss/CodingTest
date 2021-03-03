c2121674b309138edf49121e016dac9f58e08313
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-03-03 19:29:41 +0900

    Add files via upload

---
+++ ifFive.java
@@ -0,0 +1,37 @@
+package com.secondd;
+
+import java.util.Scanner;
+
+public class ifFive {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if(m>=45) {
+			m = m-45;
+			System.out.println(h+" "+m);
+		} else {
+			if(h==0) {
+				h = 23;
+				m = m +15;
+				System.out.println(h+" "+m);
+			} else {
+				h = h-1;
+				m = m + 15;
+				System.out.println(h+ " " +m);
+			}
+		}
+		
+		
+	}
+}
+
+
+
+
+
+
---
+++ ifThree.java
@@ -0,0 +1,23 @@
+package com.secondd;
+
+import java.util.Scanner;
+
+public class ifThree {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		
+		if(year%4==0 && !(year%100==0) || (year%400==0)) {
+			System.out.println("1");
+			
+		} else {
+			System.out.println("0");
+		}
+		
+		
+	}
+
+}
