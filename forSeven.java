ce214e02f460456cf4a0a385fdd8c883fb323801
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-03-04 23:41:13 +0900

    Add files via upload

---
+++ forSeven.java
@@ -0,0 +1,74 @@
+package com.three;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class forSeven {
+
+	public static void main(String[] args) {
+		
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			int a = Integer.parseInt(reader.readLine());
+			
+			
+			
+			
+			
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i=1; i<=a; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.printf("Case #%d: %d",i,A+B);
+			System.out.println();
+		}
+		
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st;
+		try {
+			int a = Integer.parseInt(reader.readLine());
+			for(int i = 1; i<= a; i++) {
+				st = new StringTokenizer(reader.readLine()," ");
+				System.out.println("Case #"+i+": "
+				+(Integer.parseInt(st.nextToken())
+				+Integer.parseInt(st.nextToken())));
+				
+				
+			}
+			
+			
+			
+			
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+	*/
+		
+	}
+	
+}
